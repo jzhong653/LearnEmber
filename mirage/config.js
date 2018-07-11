6368766ac5d61d891f94de0d6483d50d776989aa
Joseph <39915899+jzhong653@users.noreply.github.com>
2018-07-11 13:54:47 -0500

    Formatting issues

--- mirage/config.js
+++ mirage/config.js
@@ -1,45 +1,45 @@
 export default function() {
-  this.namespace= '/api';
+  this.namespace = '/api';
 
   this.get('/rentals',function(){
     return {
       data: [{
         type: 'rentals',
-      id: 'grand-old-mansion',
-      attributes: {
-      title: 'Grand Old Mansion',
-      owner: 'Andrew Li',
-      city: 'Andrew',
-      category: 'Andrew Houses',
-      bedrooms: 20,
-      image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
-      description: 'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.'
-    }
-  }, {
-    type: 'rentals',
-      id: 'urban-living',
-      attributes: {
-      title: 'Urban Living',
-      owner: 'Andrew Li',
-      city: 'San Francisco',
-      category: 'Andrwe Houses',
-      bedrooms: 1,
-      image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg',
-      description: 'A commuters dream. This rental is within walking distance of 2 bus stops and the Metro.'
-    }
-  }, {
-    type: 'rentals',
-      id: 'downtown-charm',
-      attributes{
-      title: 'Downtown Charm',
-      owner: 'Andrew Li',
-      city: 'Seattle',
-      category: 'Andrew Houses',
-      bedrooms: 4,
-      image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg',
-      description: 'Convenience is at your doorstep with this charming downtown rental. Great restaurants and active night life are withing a few feet.'
-    }
-  }]
-};
-})
+        id: 'grand-old-mansion',
+        attributes: {
+          title: 'Grand Old Mansion',
+          owner: 'Andrew Li',
+          city: 'Andrew',
+          category: 'Andrew Houses',
+          bedrooms: 20,
+          image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
+          description: 'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.'
+        }
+      }, {
+        type: 'rentals',
+        id: 'urban-living',
+        attributes: {
+          title: 'Urban Living',
+          owner: 'Andrew Li',
+          city: 'San Francisco',
+          category: 'Andrwe Houses',
+          bedrooms: 1,
+          image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg',
+          description: 'A commuters dream. This rental is within walking distance of 2 bus stops and the Metro.'
+        }
+      }, {
+        type: 'rentals',
+        id: 'downtown-charm',
+        attributes{
+          title: 'Downtown Charm',
+          owner: 'Andrew Li',
+          city: 'Seattle',
+          category: 'Andrew Houses',
+          bedrooms: 4,
+          image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg',
+          description: 'Convenience is at your doorstep with this charming downtown rental. Great restaurants and active night life are withing a few feet.'
+        }
+      }]
+    };
+  })
 }
