ff75a55487c334df9a88029aa78ce64707e94870
Joseph <39915899+jzhong653@users.noreply.github.com>
2018-07-11 13:56:58 -0500

    Code looks right why now working?

--- mirage/config.js
+++ mirage/config.js
@@ -1,7 +1,7 @@
 export default function() {
   this.namespace = '/api';
 
-  this.get('/rentals',function(){
+  this.get('/rentals', function() {
     return {
       data: [{
         type: 'rentals',
@@ -13,7 +13,6 @@ export default function() {
           category: 'Andrew Houses',
           bedrooms: 20,
           image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
-          description: 'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.'
         }
       }, {
         type: 'rentals',
@@ -25,7 +24,6 @@ export default function() {
           category: 'Andrwe Houses',
           bedrooms: 1,
           image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg',
-          description: 'A commuters dream. This rental is within walking distance of 2 bus stops and the Metro.'
         }
       }, {
         type: 'rentals',
@@ -37,9 +35,8 @@ export default function() {
           category: 'Andrew Houses',
           bedrooms: 4,
           image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg',
-          description: 'Convenience is at your doorstep with this charming downtown rental. Great restaurants and active night life are withing a few feet.'
         }
       }]
     };
-  })
+  });
 }
