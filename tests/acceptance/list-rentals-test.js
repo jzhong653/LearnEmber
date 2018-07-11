a261618bd781f44d9bea90d1f0e8da893ea7e28f
Joseph <39915899+jzhong653@users.noreply.github.com>
2018-07-11 13:25:37 -0500

    Learning and creating a website at the same time

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,6 +1,7 @@
 import { module, test } from 'qunit';
-import { visit, currentURL } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
+import {click, currentURL, visit} from'@ember/test-helpers'
+
 
 module('Acceptance | list rentals', function(hooks) {
   setupApplicationTest(hooks);
@@ -11,12 +12,20 @@ module('Acceptance | list rentals', function(hooks) {
   });
 
   test('should link to information about the company', async function (assert) {
+    await visit('/');
+    await click(".menu-about");
+    assert.equal(currentURL(), '/about', 'should navigate to about');
   });
 
   test('should link to contact information.', async function (assert) {
+    await visit('/');
+    await click(".menu-contact");
+    assert.equal(currentURL(), '/contact', 'should navigate to contact');
   });
 
   test('should list available rentals.', async function (assert){
+    await visit('/');
+    assert.equal(this.element.querySelectorAll('.listing').length, 3, 'should display 3 listings');
   });
 
   test('should filter the list of rentals by city.', async function (assert){
