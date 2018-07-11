042193cee6346ae5e10d67c59433d42c04614755
Joseph <39915899+jzhong653@users.noreply.github.com>
2018-07-11 13:36:38 -0500

    Added Mirage coding

--- mirage/config.js
+++ mirage/config.js
@@ -1,26 +1,36 @@
 export default function() {
+  this.namespace= '/api';
 
-  // These comments are here to help you get started. Feel free to delete them.
-
-  /*
-    Config (with defaults).
-
-    Note: these only affect routes defined *after* them!
-  */
-
-  // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
-  // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-
-  /*
-    Shorthand cheatsheet:
-
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-
-    http://www.ember-cli-mirage.com/docs/v0.3.x/shorthands/
-  */
+  this.get('/rentals',function(){
+    return {
+      id: 'grand-old-mansion',
+      title: 'Grand Old Mansion',
+      owner: 'Andrew Li',
+      city: 'Andrew',
+      category: 'Andrew Houses',
+      bedrooms: 20,
+      image: 'https://upload.wikimedia.org/wikipedia/commons/c/cb/Crane_estate_(5).jpg',
+      description: 'This grand old mansion sits on over 100 acres of rolling hills and dense redwood forests.'
+    }, {
+      id: 'urban-living',
+      title: 'Urban Living',
+      owner: 'Andrew Li',
+      city: 'San Francisco',
+      category: 'Andrwe Houses',
+      bedrooms: 1,
+      image: 'https://upload.wikimedia.org/wikipedia/commons/0/0e/Alfonso_13_Highrise_Tegucigalpa.jpg',
+      description: 'A commuters dream. This rental is within walking distance of 2 bus stops and the Metro.'
+    }, {
+      id: 'downtown-charm',
+      title: 'Downtown Charm',
+      owner: 'Andrew Li',
+      city: 'Seattle',
+      category: 'Andrew Houses',
+      bedrooms: 4,
+      image: 'https://upload.wikimedia.org/wikipedia/commons/f/f7/Wheeldon_Apartment_Building_-_Portland_Oregon.jpg',
+      description: 'Convenience is at your doorstep with this charming downtown rental. Great restaurants and active night life are withing a few feet.'
+    }
+  }]
+};
+})
 }
